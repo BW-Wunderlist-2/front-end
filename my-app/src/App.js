adc2278278e74414d7dc49822228e1bf7214bde4
EPCornwall <Elizabeth.cornwall91@gmail.com>
2020-09-24 01:13:44 -0500

    committing before merging

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState}from 'react';
 import './App.css';
 import {Route} from 'react-router-dom'
 import {PrivateRoute, SignUp, Login, LandingPage} from './components'
 
 
 function App() {
+
+  const [formValue, setFormValue] = useState({
+    name: "",
+    password: "",
+    email: "",
+    cbx: false
+
+})
+
+const [errors, setErrors] = useState({
+    name: "",
+    password: "",
+    email: ""
+
+})
+
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <Route path='/signup' component={SignUp} />
-      <Route path='/login' component={Login} />
+      <Route path='/signup' component={() => <SignUp formValue={formValue} setFormValue={setFormValue} errors={errors} setErrors={setErrors} />} />
+      <Route path='/login' component={() =><Login formValue={formValue} setFormValue={setFormValue} errors={errors} setErrors={setErrors} />} />
       <PrivateRoute path='/home' component={LandingPage} />
     </div>
   );
